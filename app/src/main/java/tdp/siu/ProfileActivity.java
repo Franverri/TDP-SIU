69ebbe51d5cb36333ce74a5d6390b6d404766601
Franverri <franverri@hotmail.com>
2018-09-21 11:46:02 -0300

    Vuelta atras desde el perfil

--- app/src/main/java/tdp/siu/ProfileActivity.java
+++ app/src/main/java/tdp/siu/ProfileActivity.java
@@ -1,7 +1,10 @@
 package tdp.siu;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -17,6 +20,18 @@ public class ProfileActivity extends AppCompatActivity {
 
         setTitle("Perfil");
 
+        //Agrego la flecha de volver atras en la barra superior
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_profile);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        //TO DO: Validar si es alumno o docente!
+        Intent intent = new Intent(this, MainActivityAlumno.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        return true;
     }
 }
