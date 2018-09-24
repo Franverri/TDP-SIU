0c4682e49d6249219e321116f17904a38bcd8b5b
Franverri <franverri@hotmail.com>
2018-09-24 13:57:51 -0300

    Card para Inscripciones

--- app/src/main/java/tdp/siu/InscripcionesActivity.java
+++ app/src/main/java/tdp/siu/InscripcionesActivity.java
@@ -3,11 +3,20 @@ package tdp.siu;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InscripcionesActivity extends AppCompatActivity {
+
+    List<Inscripcion> inscripcionList;
+    InscripcionAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +31,30 @@ public class InscripcionesActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         setContentView(R.layout.activity_inscripciones);
+
+        configurarRecyclerView();
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
         super.onBackPressed();
         return true;
+    }
+
+    private void configurarRecyclerView() {
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView_inscripciones);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        inscripcionList = new ArrayList<>();
+
+        //creating recyclerview adapter
+        adapter = new InscripcionAdapter(this, inscripcionList);
+
+        //FALTARIA SINCRONIZAR CON LA API
+        inscripcionList.add(new Inscripcion("TDP2", "7546", "Fontela", "Lunes 17:00 - 23:00"));
+        inscripcionList.add(new Inscripcion("Algo 3", "7504", "Fontela", "Martes 17:00 - 20:000 \n  Jueves 17:00 - 20:00"));
+        recyclerView.setAdapter(adapter);
     }
 }
