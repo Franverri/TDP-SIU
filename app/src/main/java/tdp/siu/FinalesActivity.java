7191d23296db817313ab16ac89a13c01be8797df
Franverri <franverri@hotmail.com>
2018-10-21 12:22:25 -0300

    Pantalla inicial OfertaFinales

--- app/src/main/java/tdp/siu/FinalesActivity.java
+++ app/src/main/java/tdp/siu/FinalesActivity.java
@@ -2,32 +2,25 @@ package tdp.siu;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,6 +63,23 @@ public class FinalesActivity extends AppCompatActivity {
         configurarHTTPRequestSingleton();
 
         configurarRecyclerView();
+
+        configurarClickNuevoFinal();
+    }
+
+    private void configurarClickNuevoFinal() {
+        Button button = (Button) findViewById(R.id.btn_nuevoFinal);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goOfertaFinal();
+            }
+        });
+    }
+
+    private void goOfertaFinal() {
+        Intent intent = new Intent(this, OfertaFinalesActivity.class);
+        startActivity(intent);
     }
 
     private void configurarHTTPRequestSingleton() {
---
+++ app/src/main/java/tdp/siu/OfertaFinalesActivity.java
@@ -0,0 +1,13 @@
+package tdp.siu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OfertaFinalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oferta_finales);
+    }
+}
