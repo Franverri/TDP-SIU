bfeb2a5c376d6d63d013618cbc6bd71a1c541ba2
Franverri <franverri@hotmail.com>
2018-10-26 13:40:59 -0300

    Muestra notificacion en la barra

--- app/src/main/java/tdp/siu/MyFirebaseMessagingService.java
+++ app/src/main/java/tdp/siu/MyFirebaseMessagingService.java
@@ -1,5 +1,12 @@
 package tdp.siu;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -15,7 +22,33 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         if(remoteMessage.getNotification() != null){
             Log.d("FIREBASEE", "Notificacion: " + remoteMessage.getNotification().getBody());
+
+            mostrarNotificacion(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
         }
 
+        if(remoteMessage.getData().size() > 0){
+            Log.d("FIREBASEE", "Data: " + remoteMessage.getData());
+        }
+
+    }
+
+    private void mostrarNotificacion(String title, String body) {
+
+        Intent intent = new Intent(this, MainActivityAlumno.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_logo_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
