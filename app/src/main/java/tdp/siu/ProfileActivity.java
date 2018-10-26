98ddfed4f9268ad1bcde8adbc98a254c21780c4c
Franverri <franverri@hotmail.com>
2018-10-26 14:32:29 -0300

    A5 - Configuración de endpoint E17

--- app/src/main/java/tdp/siu/ProfileActivity.java
+++ app/src/main/java/tdp/siu/ProfileActivity.java
@@ -1,5 +1,6 @@
 package tdp.siu;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,12 +8,26 @@ import android.os.TestLooperManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -21,6 +36,10 @@ public class ProfileActivity extends AppCompatActivity {
     private boolean esAlumno;
 
     TextView tvNombre, tvDNI, tvPadron, tvMail, tvPSW;
+
+    RequestQueue queue;
+    ProgressDialog progress;
+    String APIUrl ="https://siu-api.herokuapp.com/perfil";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +65,28 @@ public class ProfileActivity extends AppCompatActivity {
 
         //Determinar si colocar Padron o Legajo según el usuario
         configurarAtributos();
+
+        configurarHTTPRequestSingleton();
+    }
+
+    private void configurarHTTPRequestSingleton() {
+
+        // Get RequestQueue Singleton
+        queue = HTTPRequestSingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        queue = new RequestQueue(cache, network);
+
+        // Start the queue
+        queue.start();
+
     }
 
     private void configurarAtributos() {
@@ -84,6 +125,32 @@ public class ProfileActivity extends AppCompatActivity {
         //TO DO: hacer llamada a API para modificar los datos
         Toast.makeText(this, "Cambios guardados!",
                 Toast.LENGTH_LONG).show();
+        /*
+        String url = APIUrl + "?mail={nuevo_mail}&pswactual={actual_psw}&pswnueva={nueva_psw}";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.PUT, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        progress.dismiss();
+                        Log.i("API","Response: " + response.toString());
+                        //procesarRespuesta(response);
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progress.dismiss();
+                        Log.i("Error.Response", String.valueOf(error));
+                        Toast.makeText(ProfileActivity.this, "No fue posible conectarse al servidor, por favor intente más tarde",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);*/
+
         super.onBackPressed();
     }
 }
