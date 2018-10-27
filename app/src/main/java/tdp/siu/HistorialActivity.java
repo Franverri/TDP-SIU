b4a04c616d92fcbe982ed526b79bd015679f6102
Franverri <franverri@hotmail.com>
2018-10-27 19:14:56 -0300

    A8 - Estructura inicial del layout

--- app/src/main/java/tdp/siu/HistorialActivity.java
+++ app/src/main/java/tdp/siu/HistorialActivity.java
@@ -1,13 +1,25 @@
 package tdp.siu;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class HistorialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove notification bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Agrego la flecha de volver atras en la barra superior
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_historial);
     }
 }
