c87101b49c331d1370f33f0847ba2b001274748c
Franverri <franverri@hotmail.com>
2018-11-09 13:07:29 -0300

    A9 - Almacena historial de notificaciones

--- app/src/main/java/tdp/siu/MyFirebaseMessagingService.java
+++ app/src/main/java/tdp/siu/MyFirebaseMessagingService.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -13,9 +14,17 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editorShared;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        //SharedPref para almacenar datos de sesión
+        sharedPref = getSharedPreferences(getString(R.string.saved_data), Context.MODE_PRIVATE);
+        editorShared = sharedPref.edit();
 
         String from = remoteMessage.getFrom();
         Log.d("FIREBASEE", "Mensaje recibido de: " + from);
@@ -33,6 +42,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void mostrarNotificacion(String title, String body) {
+
+        String strNotificaciones = sharedPref.getString("strNotificaciones", "");
+        strNotificaciones = strNotificaciones + body + ";";
+        editorShared.putString("strNotificaciones", strNotificaciones);
+        editorShared.apply();
 
         Intent intent = new Intent(this, MainActivityAlumno.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
--- app/src/main/java/tdp/siu/NotificacionesActivity.java
+++ app/src/main/java/tdp/siu/NotificacionesActivity.java
@@ -1,5 +1,7 @@
 package tdp.siu;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +16,16 @@ import java.util.List;
 
 public class NotificacionesActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editorShared;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //SharedPref para almacenar datos de sesión
+        sharedPref = getSharedPreferences(getString(R.string.saved_data), Context.MODE_PRIVATE);
+        editorShared = sharedPref.edit();
 
         //Remove notification bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -27,14 +36,22 @@ public class NotificacionesActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         setContentView(R.layout.activity_notificaciones);
+    }
 
+    @Override
+    protected void onResume() {
         agregarNotificaciones();
+        super.onResume();
     }
 
     private void agregarNotificaciones() {
 
         //Obtengo el historial de notificaciones del SharedPref
-        String strNotificaciones = "noti 1;noti 2;noti 3;noti 4";
+        String strNotificaciones = sharedPref.getString("strNotificaciones", "");
+        if(strNotificaciones.equals("")){
+            strNotificaciones = "Sin notificaciones";
+        }
+        //String strNotificaciones = "noti 1;noti 2;noti 3;noti 4";
         List<String> listaNotificaciones = Arrays.asList(strNotificaciones.split(";"));
 
         ListView listView = (ListView) findViewById(R.id.listaNotificaciones);
