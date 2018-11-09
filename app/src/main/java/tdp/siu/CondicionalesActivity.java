6d322137ff69e6e634fd546abd25993ccff8d133
federicobalina <federicobalina@gmail.com>
2018-11-09 18:27:00 -0300

    Esqueleto condicionalesActivity

--- app/src/main/java/tdp/siu/CondicionalesActivity.java
+++ app/src/main/java/tdp/siu/CondicionalesActivity.java
@@ -1,4 +1,135 @@
 package tdp.siu;
 
-public class CondicionalesActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CondicionalesActivity extends AppCompatActivity {
+    RequestQueue queue;
+    String APIUrl ="https://siu-api.herokuapp.com/docente/";
+    int idCurso = -1;
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editorShared;
+
+    List<Alumno> alumnosList;
+    AlumnosInscriptosAdapter adapter;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Recuperar el id del curso en el cual se hizo click
+        Bundle b = getIntent().getExtras();
+        if(b != null)
+            idCurso = b.getInt("id");
+
+        //Remove notification bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Agrego la flecha de volver atras en la barra superior
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        setContentView(R.layout.activity_condicionales);
+
+        configurarHTTPRequestSingleton();
+
+        configurarRecyclerView();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void configurarRecyclerView() {
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView_fechas_examen);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        alumnosList = new ArrayList<>();
+
+        //creating recyclerview adapter
+        adapter = new AlumnosInscriptosAdapter(this, alumnosList);
+
+        //Aca se manda el request al server
+        enviarRequestGetCondicionales();
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void enviarRequestGetCondicionales() {
+        String url = APIUrl + "condicionales?id_curso=" + idCurso;
+        Log.i("API", "url: " + url);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("API","Response: " + response.toString());
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("Error.Response", String.valueOf(error));
+                        Toast.makeText(CondicionalesActivity.this, "No fue posible conectarse al servidor, por favor intente más tarde",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
+    }
+
+
+    private void configurarHTTPRequestSingleton() {
+
+        // Get RequestQueue Singleton
+        queue = HTTPRequestSingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        queue = new RequestQueue(cache, network);
+
+        // Start the queue
+        queue.start();
+    }
 }
